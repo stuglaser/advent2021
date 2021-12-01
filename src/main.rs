c89abadf059e42b3e45f2297036ff45ccad03d0c
Stuart Glaser <stuglaser@gmail.com>
2021-12-01 01:43:43 -0800

    Benchmarking code

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::io::{BufReader, BufRead};
 use std::fs::File;
 use std::time::Instant;
 
-use clap::{AppSettings, Parser};
+use clap::Parser;
 
 
 #[derive(Parser)]
@@ -10,6 +10,10 @@ struct Opts {
     #[clap(short, default_value="1")]
     repeat: i32,
     day: Option<i32>,
+    #[clap(long)]
+    atleast: Option<f32>,
+    #[clap(long)]
+    per: bool,
 }
 
 
@@ -27,6 +31,13 @@ fn day01() {
             cnt += 1;
         }
     }
+    // for (a, b) in inputs.iter().zip(inputs.iter().skip(1)) {
+    //     if b > a {
+    //         cnt += 1;
+    //     }
+    // }
+    
+
     //println!("Day 1.  Part 1: {}", cnt);
     assert_eq!(cnt, 1215);
 
@@ -44,33 +55,67 @@ fn day01() {
 }
 
 static DAYS: &'static [fn()] = &[
-    day01
+    day01,
 ];
 
 fn main() {
     let opts = Opts::parse();
+    assert!(opts.repeat == 1 || opts.atleast == None);
     println!("Hello, world!");
     match opts.day {
         Some(day) => println!("Day {}", day),
         None => println!("All days"),
     }
 
-    let started = Instant::now();
-
-    for _ in 0..opts.repeat {
-        match opts.day {
-            Some(day) => {
+    if opts.per {
+        // Benchmarks per-day.
+        let atleast = opts.atleast.unwrap_or(0.5);
+        let mut total = 0f64;
+        for i in 0..DAYS.len() {
+            let started = Instant::now();
+            let mut samples = 0;
+            while started.elapsed().as_secs_f32() < atleast {
+                DAYS[i]();
+                samples += 1;
+            }
+            let elapsed = started.elapsed();
+            total += (elapsed / samples).as_secs_f64();
+            println!("Day {:2} | {:?}  ({} samples)", i + 1, elapsed / samples, samples);
+        }
+        println!("Theoretical total: {}", total);
+    } else {  // Benchmarks the total
+        // Running one day or everything?
+        let runner: Box<dyn Fn()> = match opts.day {
+            Some(day) => Box::new(move || {
                 DAYS[day as usize - 1]();
-            },
-            None => {
+            }),
+            None => Box::new(|| {
                 for run_day in DAYS {
                     run_day();
                 }
-            }
-        }
-    }
+            })
+        };
 
-    let elapsed = started.elapsed();
-    let per_run_us = elapsed.as_micros() / opts.repeat as u128;
-    println!("Took {} us", per_run_us);
+        let started = Instant::now();
+
+        let mut repeated = 0u32;
+        match opts.atleast {
+            None =>
+                for _ in 0..opts.repeat {
+                    runner();
+                    repeated += 1;
+                },
+            Some(atleast) => {
+                while started.elapsed().as_secs_f32() < atleast {
+                    runner();
+                    repeated += 1;
+                }
+            },
+        }
+
+        let elapsed = started.elapsed();
+        println!("Took {:?}  ({} samples)", elapsed / repeated, repeated);
+        // let per_run_us = elapsed.as_micros() / opts.repeat as u128;
+        // println!("Took {} us  {:?}", per_run_us, elapsed / opts.repeat as u32);
+    }
 }
