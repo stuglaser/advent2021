f5174e112975acfeef9b7e9155baa110e8692db0
Stuart Glaser <stuglaser@gmail.com>
2021-12-02 23:01:28 -0800

    day 3, working

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::cmp::max;
 use std::io::{BufReader, BufRead};
-use std::fs::File;
+use std::fs::{File, self};
+use std::ops::Index;
 use std::time::Instant;
 
 use clap::Parser;
@@ -74,7 +76,7 @@ fn day02() {
     }
 
     let mut x = 0;
-    let mut depth = 0;
+    let mut depth = 0; 
     for (dir, dist) in &inputs {
         match dir {
             Dir::Forward => x += dist,
@@ -100,9 +102,131 @@ fn day02() {
     assert_eq!(x * depth, 1954293920);
 }
 
+
+struct Grid<T> {
+    rows: usize,
+    cols: usize,
+    data: Vec<T>,
+}
+
+impl<T> Grid<T> {
+
+}
+
+impl<T> Index<(usize, usize)> for Grid<T> {
+    type Output = T;
+
+    fn index(&self, rowcol: (usize, usize)) -> &Self::Output {
+        &self.data[rowcol.0 * self.cols + rowcol.1]
+    }
+
+}
+
+fn day03() {
+    let INPUT = "inputs/input03.txt";
+
+    // Constructs the grid manually so we can stream it in.
+    let reader = BufReader::new(File::open(INPUT).expect("Cannot open file"));
+    let mut grid_data = Vec::<u8>::with_capacity(1001 * 12);
+    let mut grid_rows = 0;
+    for line in reader.lines() {
+        for ch in line.unwrap().chars() {
+            grid_data.push((ch as u8) - b'0');
+        }
+        grid_rows += 1;
+    }
+    let grid = Grid::<u8>{rows: grid_rows, cols: grid_data.len() / grid_rows, data: grid_data};
+
+    let mut cnt_ones = vec![0; grid.cols];
+    for row in 0..grid.rows {
+        for k in 0..grid.cols {
+            if grid[(row, k)] == 1 {
+                cnt_ones[k] += 1;
+            }
+        }
+    }
+
+    let mut epsilon = 0;
+    let mut gamma = 0;
+    for k in 0..cnt_ones.len() {
+        gamma <<= 1;
+        epsilon <<= 1;
+        if cnt_ones[k] > grid.rows / 2 {
+            gamma += 1;
+        } else {
+            epsilon += 1;
+        }
+    }
+    //println!("Part 1: {}  (because {:?})", gamma * epsilon, cnt_ones);
+    assert_eq!(gamma * epsilon, 3959450);
+
+    // Oxygen
+    let mut keep = vec![true; grid.rows];
+    let mut value: i32 = if cnt_ones[0] > grid.rows / 2 { 1 } else { 0 };
+    for k in 1..cnt_ones.len() {
+        let mut next_ones: usize = 0;
+        let mut next_total: usize = 0;
+        for row in 0..grid.rows {
+            if keep[row] {
+                if grid[(row, k - 1)] == (value & 0b1) as u8 {
+                    // Keep keeping, and count this one
+                    next_ones += grid[(row, k)] as usize;
+                    next_total += 1;
+                } else {
+                    keep[row] = false;
+                }
+            }
+        }
+
+        value <<= 1;
+        if next_ones * 2 >= next_total { value += 1; }
+    }
+    let oxygen = value;
+
+
+    // CO2
+    let mut keep = vec![true; grid.rows];
+    let mut value: i32 = if !(cnt_ones[0] > grid.rows / 2) { 1 } else { 0 };
+    for k in 1..cnt_ones.len() {
+        let mut next_ones: usize = 0;
+        let mut next_total: usize = 0;
+        let mut kept_row = 0;
+        for row in 0..grid.rows {
+            if keep[row] {
+                if grid[(row, k - 1)] == (value & 0b1) as u8 {
+                    // Keep keeping, and count this one
+                    next_ones += grid[(row, k)] as usize;
+                    next_total += 1;
+                    kept_row = row;
+                } else {
+                    keep[row] = false;
+                }
+            }
+        }
+
+        // Early exit if there's one value left
+        if next_total == 1 {
+            value = 0;
+            for c in 0..grid.cols {
+                value <<= 1;
+                value += grid[(kept_row, c)] as i32;
+            }
+            break;
+        }
+
+        value <<= 1;
+        if next_ones * 2 < next_total { value += 1; }
+    }
+    let co2 = value;
+
+    //println!("Part 2: {}", oxygen * co2);
+    assert_eq!(oxygen * co2, 7440311);
+}
+
 static DAYS: &'static [fn()] = &[
     day01,
     day02,
+    day03,
 ];
 
 fn main() {
@@ -129,7 +253,7 @@ fn main() {
             total += (elapsed / samples).as_secs_f64();
             println!("Day {:2} | {:?}  ({} samples)", i + 1, elapsed / samples, samples);
         }
-        println!("Theoretical total: {}", total);
+        println!("Theoretical total: {} ms", total * 1000.0);
     } else {  // Benchmarks the total
         // Running one day or everything?
         let runner: Box<dyn Fn()> = match opts.day {
