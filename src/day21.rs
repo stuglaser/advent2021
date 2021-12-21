327d5f776be17bec3875e40a3c2586998cda66dc
Stuart Glaser <stuglaser@gmail.com>
2021-12-20 22:14:30 -0800

    day21, 203ms -> 1.23ms

--- src/day21.rs
+++ src/day21.rs
@@ -1,5 +1,9 @@
 use std::cmp::{min, max};
 
+use rustc_hash::FxHashMap;
+
+type AnswerCache = FxHashMap<(i32, i32, i32, i32), (usize, usize)>;
+
 struct DeterministicDie {
     value: i32,
     rolls: usize,
@@ -27,19 +31,25 @@ const ROLLS3: [(i32, usize); 7] = [
     (9, 1),
 ];
 
-fn sim(at1: i32, score1: i32, at2: i32, score2: i32) -> (usize, usize) {
+fn sim(cache: &mut AnswerCache, at1: i32, score1: i32, at2: i32, score2: i32) -> (usize, usize) {
     if score1 >= 21 { return (1, 0); }
     if score2 >= 21 { return (0, 1); }
+
+    if let Some(result) = cache.get(&(at1, score1, at2, score2)) {
+        return *result;
+    }
 
     let mut wins1 = 0;
     let mut wins2 = 0;
 
     for (mv, times) in ROLLS3 {
         let at_next = (at1 + mv - 1) % 10 + 1;
-        let (w2, w1) = sim(at2, score2, at_next, score1 + at_next);
+        let (w2, w1) = sim(cache, at2, score2, at_next, score1 + at_next);
         wins1 += w1 * times;
         wins2 += w2 * times;
     }
+
+    cache.insert((at1, score1, at2, score2), (wins1, wins2));
     (wins1, wins2)
 }
 
@@ -84,7 +94,8 @@ pub fn day21(test_mode: bool) {
     // println!("Part 1: {}", part1);
     assert_eq!(part1, if test_mode { 739785 } else { 920079 });
 
-    let (wins1, wins2) = sim(p1_start, 0, p2_start, 0);
+    let mut cache = AnswerCache::with_capacity_and_hasher(10000, Default::default());
+    let (wins1, wins2) = sim(&mut cache, p1_start, 0, p2_start, 0);
     // println!("WINS: {}, {}", wins1, wins2);
 
 
